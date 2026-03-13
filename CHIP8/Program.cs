7c04533d0b489c165dac3a80210ac8f24a1a4977
NightRider92 <moran.rod@gmail.com>
2026-03-13 07:16:03 +0100

    Optimize CPU and timer processing with Stopwatch

--- CHIP8/Program.cs
+++ CHIP8/Program.cs
@@ -1,11 +1,28 @@
-﻿
+
 using CHIP8;
 using CHIP8.CPU;
+using System.Diagnostics;
+
+Stopwatch stopwatchCPU = new Stopwatch();
+Stopwatch stopwatchTimers = new Stopwatch();
+
+stopwatchCPU.Start();
+stopwatchTimers.Start();    
 
 Core system = new Core("outlaw.ch8");
 Console.Clear();
+
 while (true)
 {
-    system.Process();
-    //Thread.Sleep(1); 
+    if(stopwatchCPU.Elapsed.TotalMilliseconds > 1) // ~ 1000Hz
+    {
+        system.Process();
+        stopwatchCPU.Restart();
+    }
+
+    if (stopwatchCPU.Elapsed.TotalMilliseconds > 16.67f) // ~ 60Hz
+    {
+        system.ProcessTimers();
+        stopwatchTimers.Restart();
+    }
 }
